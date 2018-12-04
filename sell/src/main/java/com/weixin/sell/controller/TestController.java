b899b3aa7a5bd0d401c14be38dbfdc370c6b645f
daiweidong <790335270@qq.com>
2018-12-04 13:47:50 +0800

    新增测试controller

---
+++ sell/src/main/java/com/weixin/sell/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.weixin.sell.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test/controller")
+public class TestController {
+
+}
